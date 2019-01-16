e553ea9bed0fcece0b3116165a538f8bb6da0ff5
sungminryu <segost@daum.net>
2019-01-16 19:55:54 +0900

    Project setting

---
+++ src/test/java/com/wua/wiseup/WiseupApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wua.wiseup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WiseupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
