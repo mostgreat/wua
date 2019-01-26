9fe5d4c1341a61750ff4074fffd204fee7b2ee2b
sungminryu <segost@daum.net>
2019-01-26 14:30:30 +0900

    api 호출 해서 받아보고 싶은데 문서보고 수정 시작하려다가 멈춤. 안쓰는 클래스 몇 개 추가했는데 다음 커밋에서 변경

--- src/main/java/com/wua/wiseup/controller/BootStrapImage.java
+++ src/main/java/com/wua/wiseup/controller/BootStrapImage.java
@@ -1,13 +1,31 @@
 package com.wua.wiseup.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Controller
 public class BootStrapImage {
 
     @GetMapping("/bootStrap")
     public String showBootStrap() {
         return "bootStrap.html";
+    }
+
+    @RequestMapping("/get")
+    public void get(){
+        String testUrl = "https://openapi.naver.com/v1/datalab/shopping/category/device";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(testUrl, String.class); // TODO String 말고
+    }
+
+    private String jsonStringFromObject(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
